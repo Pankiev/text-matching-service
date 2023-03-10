e7c7a1a00624f957a6046bb84543ea81f2e32eb5
Pankiev <farami11@tlen.pl>
2023-03-10 23:25:07 +0100

    Project init

---
+++ src/main/java/com/cdq/textmatching/TextMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.cdq.textmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextMatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdq/textmatching/TextMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdq.textmatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
